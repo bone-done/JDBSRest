442372a907114f15366377ee508ac6f1cbdd5b6f
Mosklajuk Yurij <dubljanskyj@gmail.com>
2021-12-17 00:05:43 +0200

    added user methods

--- src/main/java/com/bonedone/util/Queries.java
+++ src/main/java/com/bonedone/util/Queries.java
@@ -6,6 +6,6 @@ public class Queries {
     public static final String CREATE_USER = "INSERT INTO user (email, password, fullname, lastname, role, id) VALUES (?,?,?,?,?,?)";
     public static final String UPDATE_USER = "UPDATE user SET email = ?, password = ?, fullname = ?, lastname = ?, role = ? WHERE id = ?";
     public static final String DELETE_USER = "DELETE FROM user WHERE id = ?";
-    public static final String USER_IS_EXIST = "SELECT * FROM STUDENT WHERE ID = ?"
+    public static final String USER_IS_EXIST = "SELECT * FROM STUDENT WHERE ID = ?";
 
 }
